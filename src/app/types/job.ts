cbcee1d1803a2b5562585a97404b8608eb010ebf
Linh <nguyenthilinhqni2020@gmail.com>
2025-06-26 18:35:17 +0700

    display job and carousel

---
+++ src/app/types/job.ts
@@ -0,0 +1,14 @@
+export interface Job {
+  id: number;
+  title: string;
+  company: string;
+  location: string;
+  type: string;
+  salary: string;
+  description: string;
+  requirements: string[];
+  benefits: string[];
+  postedDate: string;
+  deadline: string;
+  status: string;
+}
---
+++ src/components/Banner/Marquee.tsx
@@ -0,0 +1,11 @@
+export default function Marquee() {
+  return (
+    <div className="w-full overflow-hidden bg-blue-600 p-4 border-y ">
+      <p className="whitespace-nowrap animate-marquee pause-on-hover text-lg font-semibold text-white text-center">
+        🚀 Tìm việc dễ dàng, ứng tuyển chỉ với 1 cú click! &nbsp;&nbsp;&nbsp; 💼
+        Kết nối nhanh với nhà tuyển dụng uy tín! &nbsp;&nbsp;&nbsp; 📌 Việc làm
+        mơ ước không còn xa – Khám phá ngay!
+      </p>
+    </div>
+  );
+}
